37b5b6307cc13e514bd97b8487f8d3c915321636
Armando Azvedo <armandojpazevedo@gmail.com>
2022-05-23 19:09:11 +0100

    SQL OnCreate e Atividade adicionar filmes

---
+++ app/src/main/java/com/example/gestovideoclube/adicionar_filmes.kt
@@ -0,0 +1,11 @@
+package com.example.gestovideoclube
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class adicionar_filmes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_filmes)
+    }
+}
