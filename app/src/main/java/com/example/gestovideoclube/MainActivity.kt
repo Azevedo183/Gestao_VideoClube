b6d3d70c0b7819a440d1304ed7d916bcd593100e
Armando Azvedo <armandojpazevedo@gmail.com>
2022-05-24 06:37:41 +0100

    Navegação concluida

--- app/src/main/java/com/example/gestovideoclube/MainActivity.kt
+++ app/src/main/java/com/example/gestovideoclube/MainActivity.kt
@@ -2,19 +2,33 @@ package com.example.gestovideoclube
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        //val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
-        //val navController = findNavController(R.id.fragmentContainerView)
+        val navigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        navigationView.selectedItemId = R.id.filmes
 
-        //bottomNavigationView.setupWithNavController(navController)
+        val navController = findNavController(R.id.fragmentfragment)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.filmes, R.id.requesitados, R.id.clientes))
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        bottomNavigationView.setupWithNavController(navController)
+
+
     }
 
 }
